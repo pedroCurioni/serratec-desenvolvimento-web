78ddd12cf3ad0ec827f4b70e200a693ff5897159
pedroCurioni <pedrocurioni@gmail.com>
2022-06-13 19:04:48 -0300

    refractorando pastas em componentes

--- src/Routes/root.js
+++ src/Routes/root.js
@@ -2,13 +2,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // import "./App.css";
-// import {ComponentePai} from "./pages/aula02/ComponentePai"
-// import { AdicionarLista } from "./pages/aula03/AdicionarLista";
-import { RendereizacaoCondicional } from "../pages/aula03/RenderizacaoCondicional";
-import { AdicionarLista } from "../pages/aula03/AdicionarLista";
-import { Home } from "../pages/aula03/Home";
-import { QuemSomos } from "../pages/aula03/QuemSomos";
-import { ComponentePai } from "../pages/aula02/ComponentePai";
+// import {ComponentePai} from "./Compnents/aula02/ComponentePai"
+// import { AdicionarLista } from "./Compnents/aula03/AdicionarLista";
+import { RendereizacaoCondicional } from "../Compnents/aula03/RenderizacaoCondicional";
+import { AdicionarLista } from "../Compnents/aula03/AdicionarLista";
+import { Home } from "../Compnents/aula03/Home";
+import { QuemSomos } from "../Compnents/aula03/QuemSomos";
+import { ComponentePai } from "../Compnents/aula02/ComponentePai";
 
 export const Root = () => {
   return (
